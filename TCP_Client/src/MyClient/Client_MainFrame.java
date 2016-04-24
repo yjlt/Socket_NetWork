cd9ced22362486ee9763e038483ef0c5ef0cf8c0
yjlt <zmh1992819@163.com>
2016-04-24 11:14:31 +0800

    20160424-1
    4月24日第一次第一次修改

--- TCP_Client/src/MyClient/Client_MainFrame.java
+++ TCP_Client/src/MyClient/Client_MainFrame.java
@@ -3,6 +3,8 @@ package MyClient;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,19 +13,37 @@ import javax.swing.JLabel;
 import java.awt.TextField;
 import java.awt.Button;
 import javax.swing.JRadioButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Client_MainFrame extends JFrame {
 
 	private JPanel contentPane;
-	private String dst_ip;                             //定义目的IP
-	private int dst_port;                             //定义目的Port
+	private static String dst_ip;                             //定义目的IP
+	private static int dst_port;                             //定义目的Port
+	private File sendfile;
 	private String sendfile_path;                //要发送的文件路径
-	private int sendbuffersize;                  //定义发送缓冲区的大小
-	private int send_SoLinger;                   //定义滞留时间(ms)
-	private int send_Timeout;                   //定义超时时间(ms)
-	private boolean NagleState;              //Nagle算法状态(启用/禁止)
+	private static int sendbuffersize;                  //定义发送缓冲区的大小
+	private static int send_SoLinger;                   //定义滞留时间(s)
+	private static int send_Timeout;                   //定义超时时间(ms)
+	private static boolean NagleState;              //Nagle算法状态(启用/禁止)
 	private String send_infoback;             //反馈的信息
+	
+	private static Socket client;                         //定义socket
 
+	private static TextField dst_ip_textfield ;
+	private static TextField dst_port_textfield;
+	private static TextField sendbuffersize__textfield;
+	private static TextField send_SoLinger_textfield;
+	private static TextField send_Timeout__textfield;
+	private static JRadioButton nagle_on_radiobtn;
+	private static JRadioButton nagle_off_radiobtn;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -33,13 +53,15 @@ public class Client_MainFrame extends JFrame {
 				try {
 					Client_MainFrame frame = new Client_MainFrame();
 					frame.setVisible(true);
+					getparams(client);                                    //为client赋值
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
+	
+	
 	/**
 	 * Create the frame.
 	 */
@@ -59,7 +81,7 @@ public class Client_MainFrame extends JFrame {
 		lblip.setBounds(24, 21, 74, 23);
 		contentPane.add(lblip);
 		
-		TextField dst_ip_textfield = new TextField();
+		dst_ip_textfield = new TextField();
 		dst_ip_textfield.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		dst_ip_textfield.setBounds(121, 21, 162, 23);
 		contentPane.add(dst_ip_textfield);
@@ -69,7 +91,7 @@ public class Client_MainFrame extends JFrame {
 		label.setBounds(24, 66, 74, 23);
 		contentPane.add(label);
 		
-		TextField dst_port_textfield = new TextField();
+		dst_port_textfield = new TextField();
 		dst_port_textfield.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		dst_port_textfield.setBounds(121, 66, 162, 23);
 		contentPane.add(dst_port_textfield);
@@ -85,6 +107,16 @@ public class Client_MainFrame extends JFrame {
 		contentPane.add(file_send_textfield);
 		
 		Button openfile_btn = new Button("\u6D4F\u89C8");
+		openfile_btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				    JFileChooser jfc=new JFileChooser();  
+			        jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES );  
+			        jfc.showDialog(new JLabel(), "选择要发送的文件·	");  
+			        sendfile=jfc.getSelectedFile();                                //获得选中要发送的文件
+			        file_send_textfield.setText(sendfile.getAbsolutePath());
+			}
+		});
 		openfile_btn.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		openfile_btn.setBounds(299, 117, 76, 23);
 		contentPane.add(openfile_btn);
@@ -94,12 +126,12 @@ public class Client_MainFrame extends JFrame {
 		lblbyte.setBounds(24, 159, 90, 23);
 		contentPane.add(lblbyte);
 		
-		TextField sendbuffersize__textfield = new TextField();
+		sendbuffersize__textfield = new TextField();
 		sendbuffersize__textfield.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		sendbuffersize__textfield.setBounds(121, 159, 162, 23);
 		contentPane.add(sendbuffersize__textfield);
 		
-		JLabel lblms_1 = new JLabel("\u6EDE\u7559\u65F6\u95F4(ms):");
+		JLabel lblms_1 = new JLabel("\u6EDE\u7559\u65F6\u95F4(s):");
 		lblms_1.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		lblms_1.setBounds(24, 192, 90, 23);
 		contentPane.add(lblms_1);
@@ -109,12 +141,13 @@ public class Client_MainFrame extends JFrame {
 		lblms.setBounds(24, 225, 90, 23);
 		contentPane.add(lblms);
 		
-		TextField send_SoLinger_textfield = new TextField();
+		send_SoLinger_textfield = new TextField();
 		send_SoLinger_textfield.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		send_SoLinger_textfield.setBounds(121, 192, 162, 23);
 		contentPane.add(send_SoLinger_textfield);
 		
-		TextField send_Timeout__textfield = new TextField();
+		
+		send_Timeout__textfield = new TextField();
 		send_Timeout__textfield.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		send_Timeout__textfield.setBounds(121, 225, 162, 23);
 		contentPane.add(send_Timeout__textfield);
@@ -124,13 +157,18 @@ public class Client_MainFrame extends JFrame {
 		lblNagle.setBounds(24, 263, 90, 23);
 		contentPane.add(lblNagle);
 		
-		JRadioButton nagle_on_radiobtn = new JRadioButton("\u542F\u7528");
+		nagle_on_radiobtn = new JRadioButton("\u542F\u7528");
 		nagle_on_radiobtn.setBounds(121, 263, 74, 23);
 		contentPane.add(nagle_on_radiobtn);
 		
-		JRadioButton nagle_off_radiobtn = new JRadioButton("\u7981\u7528");
+		nagle_off_radiobtn = new JRadioButton("\u7981\u7528");
+		nagle_off_radiobtn.setSelected(true);
 		nagle_off_radiobtn.setBounds(209, 263, 74, 23);
 		contentPane.add(nagle_off_radiobtn);
+		
+		ButtonGroup bg=new ButtonGroup();
+		bg.add(nagle_on_radiobtn);
+		bg.add(nagle_off_radiobtn);
 		
 		JLabel label_2 = new JLabel("\u4FE1\u606F\u53CD\u9988:");
 		label_2.setFont(new Font("微软雅黑", Font.PLAIN, 13));
@@ -146,5 +184,41 @@ public class Client_MainFrame extends JFrame {
 		sendfile_btn.setFont(new Font("微软雅黑", Font.PLAIN, 13));
 		sendfile_btn.setBounds(299, 292, 76, 23);
 		contentPane.add(sendfile_btn);
+	}
+	
+	public static void getparams(Socket myclient){
+		                    //获取各参数的值
+		dst_ip = dst_ip_textfield.getText();
+		dst_port = Integer.parseInt(dst_port_textfield.getText());
+		sendbuffersize = Integer.parseInt(sendbuffersize__textfield.getText());
+		send_SoLinger =Integer.parseInt(send_SoLinger_textfield.getText());
+		send_Timeout = Integer.parseInt(send_Timeout__textfield.getText());
+		if(nagle_on_radiobtn.isSelected()==true)
+			NagleState = true;
+		else
+			NagleState = false;
+		
+		try {
+			myclient =new Socket(dst_ip, dst_port);                      //创建Socket
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("创建Socket失败");
+		}
+		
+		try {
+			myclient.setSendBufferSize(sendbuffersize);
+			myclient.setSoLinger(true, send_SoLinger);
+			myclient.setSoTimeout(send_Timeout);
+			myclient.setTcpNoDelay(NagleState);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("参数设置有错误！");
+		}
+		
 	}
 }
